1711883946420892dcd744bdf8444c506733b67f
Tarik <t2.macben@gmail.com>
2025-09-09 11:45:09 +0800

    Création des variables globales : MENU, MENU_ITEMS, LISTE

---
+++ liste_jeux.py
@@ -0,0 +1,20 @@
+# Import du module sys :
+import sys
+
+# Déclaration des variables globales :
+# Variable instructions :
+MENU = """
+MENU DES COMMANDES :
+1 - Ajouter un jeu vidéo à votre liste
+2 - Retirer un jeu vidéo à votre liste
+3 - Afficher votre liste de jeux vidéos
+4 - Supprimer la liste de jeux vidéos
+5 - Sortir du programme
+"""
+
+# Variable options à sélectionner :
+MENU_ITEMS = ["1", "2", "3", "4", "5"]
+
+# Variables liste vide au départ :
+LISTE = []
+
